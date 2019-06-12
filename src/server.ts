66c29173552666e286ad2603979beed25579d9c2
Brian Alzua <balzua@gmail.com>
2019-06-11 22:42:38 -0500

    removed compiled typescript files from git

--- src/server.ts
+++ src/server.ts
@@ -13,15 +13,10 @@ const books = [
 
 const typeDefs = gql`
   # Comments in GraphQL are defined with the hash (#) symbol.
-
-  # This "Book" type can be used in other type declarations.
   type Book {
     title: String
     author: String
   }
-
-  # The "Query" type is the root of all GraphQL queries.
-  # (A "Mutation" type will be covered later on.)
   type Query {
     books: [Book]
   }
@@ -35,8 +30,6 @@ const resolvers = {
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-// This `listen` method launches a web-server.  Existing apps
-// can utilize middleware options, which we'll discuss later.
 server.listen().then(({ url }: any) => {
     console.log(`The server is now running at: ${url}`);
 });
